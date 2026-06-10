721b9c913c7d4d24a1473e66c882abc91450a73d
Yxnger <jxnr21@gmail.com>
2026-06-10 23:32:03 +0200

    Add console output formatting and escape sequences

---
+++ 01_Language_Foundations/01_console_output_formatting.py
@@ -0,0 +1,14 @@
+"""
+PRINT STATEMENTS & CHARACTER SEQUENCES
+print('') - outputs text to the console
+\\ : backslash character, used to escape characters
+\b : backspace character, deletes the previous character
+\t : tab character, adds a horizontal tab space
+\r : carriage return, moves the cursor to the beginning of the line
+\n : newline character, moves the cursor to the next line
+"""
+# Example usage of print statements and character sequences
+print('Hello, World!')  # Output: Hello, World!
+
+#print hello world twice each on a new line
+print('Hello, World!\r\nHello, World!')
--- 01_Language_Foundations/README.md
+++ 01_Language_Foundations/README.md
@@ -1,1 +1,10 @@
-#Language Foundations - Bitwise Logic and Data Structures
+# [Language Foundations]
+Core Python syntax and bit-level logic for engineering application
+
+### Concepts Covered:
+Bitwise Manipulation: Register-level logic and masking.
+Data Structures: Implementation of Lists, Dictionaries, and Tuples.
+Numeric Precision: Handling complex numbers and typecasting.
+
+### Usage:
+`python 01_console_output_formatting.py`
