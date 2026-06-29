2191aaa8de9acef7cb0ed1f451328d41630b133d
Yxnger <jxnr21@gmail.com>
2026-06-29 13:46:16 +0200

    Implemented Practice Files

---
+++ 00_Practice/00_py_libraries_practice.py
@@ -0,0 +1,40 @@
+
+# 1. Numpy library
+from numpy import pi, sin, deg2rad
+
+
+#A function that finds the value of a sinousoidal wave at 45 degrees (Numpy library)
+def findVal(angle):
+    #convert degrees to radians
+    theta = deg2rad(angle)
+    
+    return round(sin(theta), 1) 
+
+# 2. SciPy library
+from scipy import integrate
+
+#A function that finds the area under a parabola (x^2) at the given bounds
+def findArea(lowerBound = 0, UpperBound = 0):
+    #equation: y = x^2
+    y = lambda x: x**2
+    
+    #using the integration function from scipy
+    return integrate.quad(y, lowerBound,UpperBound)
+    
+
+def main():
+    #1.
+    #angle = float(input('Enter the angle in degrees: '))
+    # print(f'The value of sinousoidal wave at {angle} \N{DEGREE SIGN} is {findVal(angle)}')
+    
+    #2. 
+    lowerBound = float(input('Enter the lower bound: '))
+    upperBound = float(input('Enter the upper bound: '))
+   
+    area = round(findArea(lowerBound, upperBound)[0],1)
+    
+    print(f'The area under the parabola from {lowerBound} to {upperBound} is {area}')
+
+if __name__ == '__main__':
+    main()
+    
---
+++ 00_Practice/libHelp.py
@@ -0,0 +1,3 @@
+#Finding the documentation of the integrate function in scipy 
+import scipy
+help(scipy.integrate.quad)
